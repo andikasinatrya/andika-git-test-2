b4b9b718c272ef32f452e647eabf96841f951159
andikasinatrya <andikasinatrya0@gmail.com>
2024-10-25 20:26:08 +0700

    menambahkan list ul

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Belajar rempting 2</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit maiores temporibus eum tempore quia?</p>
+    <ul>
+        <li>Git remote lolikon</li>
+        <li>Git remote lolikon</li>
+        <li>Git remote lolikon</li>
+    </ul>
 </body>
 </html>
